1b827d97666b207973048c192ae2c35301cddaa4
Phomint <patrick.dev.atom@gmail.com>
2022-02-10 18:47:00 -0300

    Fixing internal import

--- gowork/__init__.py
+++ gowork/__init__.py
@@ -1,6 +1,6 @@
-from safe.configs import Credentials
-from databases import AthenaGo, MysqlGo, GoQuery
-from handler import GoStorage
-from monitor import GoTimer, clear_memory
+from .safe.configs import Credentials
+from .databases import AthenaGo, MysqlGo, GoQuery
+from .handler import GoStorage
+from .monitor import GoTimer, clear_memory
 
 __all__ = [Credentials, AthenaGo, MysqlGo, GoQuery, GoStorage, GoTimer, clear_memory()]
