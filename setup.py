d17710524a06a42dd1c33693d74319aac6865477
Phomint <patrick.dev.atom@gmail.com>
2022-02-10 18:34:59 -0300

    More descriptions

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ gowork_version = (
 )
 
 setuptools.setup(
-    name="GoWork",
+    name="gowork",
     version=gowork_version,
     author="Patrick Amaral",
     author_email="patrick.dev.atom@gmail.com",
     description="Library to help track your credentials and database engines",
+    long_description=open('README.md').read(),
     url="https://github.com/Phomint/GoWork",
     packages=setuptools.find_packages(),
     include_package_data=True,
@@ -21,7 +22,9 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.6",
+    license='GPLv3',
+    keywords=['databases', 'athena', 'mysql'],
     install_requires=[
         "numpy",
         "pandas",
