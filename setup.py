12e57dd2fe6a117feb737822580a967a681a5791
Phomint <patrick.dev.atom@gmail.com>
2022-02-11 09:37:16 -0300

    More details

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 import subprocess
+import codecs
 
 gowork_version = (
     subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE)
@@ -13,7 +14,7 @@ setuptools.setup(
     author="Patrick Amaral",
     author_email="patrick.dev.atom@gmail.com",
     description="Library to help track your credentials and database engines",
-    long_description=open('README.md').read(),
+    long_description=codecs.open('README.md', encoding='utf-8').read(),
     url="https://github.com/Phomint/GoWork",
     packages=setuptools.find_packages(),
     include_package_data=True,
